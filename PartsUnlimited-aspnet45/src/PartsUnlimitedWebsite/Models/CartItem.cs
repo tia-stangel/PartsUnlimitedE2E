174cc82f1354be683aae281426b761f4dae74341
Tia Stangel <tstangel@chas.org>
2023-11-22 11:19:00 -0800

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // This is my first change.
+        // This is my second change.
         [Key]
         public int CartItemId { get; set; }
 
--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -5,6 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class Category
     {
+        // This is my third change.
         public int CategoryId { get; set; }
 
         [Required]
